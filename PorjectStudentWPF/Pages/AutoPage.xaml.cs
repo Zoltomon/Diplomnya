b2a3612735defb611a11adf0fa810a70218dddca
Zolto Zoltomon <zdorzhyiev@gmail.com>
2023-10-04 13:15:45 +0300

    проектировка дизайна приложения

---
+++ PorjectStudentWPF/Pages/AutoPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using PorjectStudentWPF.Classes;
+using System.Windows.Shapes;
+
+namespace PorjectStudentWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AutoPage.xaml
+    /// </summary>
+    public partial class AutoPage : Page
+    {
+        public AutoPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NavigateClass.navigate.Navigate(new MainPage());
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ PorjectStudentWPF/Pages/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PorjectStudentWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMain_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string hexColor = "#4B9BD6";
+                
+                SolidColorBrush colorBrush = new SolidColorBrush((Color) ColorConverter.ConvertFromString(hexColor));
+                BtnMain.Foreground = colorBrush;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnProduct_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
